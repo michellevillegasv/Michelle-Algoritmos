9e8cc13085fa9a6046ec2b099a45a19593e96a6e
michellevillegasss <michelle.villegas@correo.unimet.edu.ve>
2022-09-16 16:02:07 -0400

    Ejercicio 1 de prueba

---
+++ Ejercicios/cadenas.py
@@ -0,0 +1,8 @@
+name = (input("Enter your name: "))
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+user = (input("Enter your user name: "))
+letter = len(user)
+print(f"{user} have {letter} letter in its word")
