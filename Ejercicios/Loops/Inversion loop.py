c599d890d9b873db98a3a0029d1986b6e31980d6
michellevillegasss <michelle.villegas@correo.unimet.edu.ve>
2022-09-22 16:23:09 -0400

    Inversion loop :bulb:

--- Ejercicios/Loops/Inversion loop.py
+++ Ejercicios/Loops/Inversion loop.py
@@ -4,5 +4,4 @@ interest = float(input("Please enter the annual interes: % "))
 years = int(input("Please enter how many years are you going to be investing: "))
 for i in range(years):
     years = i+1
-    amount1 = amount*(interest/100)*years
-    print(f"\nYear {years}: {amount1}", end="$")
+    print(f"\nYear {years}: {amount*(interest/100)*years}", end="$")
